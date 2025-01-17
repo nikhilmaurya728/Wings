e19f5b72166a71e599edf792be4b096fc753c8ef
Nikhil Maurya <nikhilmaurya28072001@gmail.com>
2025-01-17 22:48:13 +0530

    a

--- class4math.js
+++
@@ -1,7 +0,0 @@
-var data = {
-  "Mathematics4": {
-    "math": '<h1>Learn 1 to 20 Table</h1> <h4>This is a table</h4> <p> Pari is a good girl but very weak, she is good and fast learner </p>',
-  },
-};
-
-generateSidebar();
