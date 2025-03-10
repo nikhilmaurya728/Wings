f05f559450896282cc1bba05a698a5b147d503b4
Nikhil Maurya <nikhilmaurya28072001@gmail.com>
2025-03-10 17:34:44 +0530

    Update homeEntranceExam.js

--- homeEntranceExam.js
+++ homeEntranceExam.js
@@ -1,7 +1,6 @@
 songs = ["audio/chapter1.mp3", "song2.mp3", "song3.mp3"];
 
 var data = {
-
   "Navodaya Vidyalaya": {
     "JNVs School 5th": '<div class="page"> <div class="a4page">Jawahar  Navodaya Vidyalaya </div> </div>',
   },
