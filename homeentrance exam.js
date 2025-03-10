0946f56a4bdc82473cb395329fe4b7fec9730328
Nikhil Maurya <nikhilmaurya28072001@gmail.com>
2025-03-10 17:35:59 +0530

    n

--- homeentrance exam.js
+++ homeentrance exam.js
@@ -2,7 +2,7 @@ songs = ["audio/chapter1.mp3", "song2.mp3", "song3.mp3"];
 
 var data = {
   "Navodaya Vidyalaya": {
-    "JNVs School 5th": '<div class="page"> <div class="a4page">Jawahar  Navodaya Vidyalaya </div> </div>',
+    "JNVs School 5th": '<div class="page"> <div class="a4page">Jawahar  Navodaya Vidyalaya for 5th </div> </div>',
   },
 };
 
